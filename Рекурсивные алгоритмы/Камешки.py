bb1bd16010c110ea7ef64399d7e6ec884e912bf9
Nokvix <igor.gass2003@mail.ru>
2024-03-27 14:33:37 +0500

    Задача "Камешки" решена

--- Рекурсивные алгоритмы/Камешки.py
+++ Рекурсивные алгоритмы/Камешки.py
@@ -1,17 +1,44 @@
-def pebble_count(n, i, counter, list_numbers):
-    if i == n - 1:
-        return counter
+def merge(left_list, right_list):
+    left_length = len(left_list)
+    right_length = len(right_list)
+    merged_list = []
 
-    for j in range(i + 1, n):
-        if list_numbers[i] >= list_numbers[j]:
-            counter += 1
-    return pebble_count(n, i + 1, counter, list_numbers)
+    counter = 0
+    i, j = 0, 0
+    while i < left_length and j < right_length:
+        if left_list[i] >= right_list[j]:
+            counter += left_length - i
+            merged_list.append(right_list[j])
+            j += 1
+        else:
+            merged_list.append(left_list[i])
+            i += 1
+
+    merged_list.extend(left_list[i:])
+    merged_list.extend(right_list[j:])
+
+    return merged_list, counter
+
+
+def list_division(list_numbers):
+    list_numbers_length = len(list_numbers)
+
+    if list_numbers_length < 2:
+        return list_numbers, 0
+
+    middle = list_numbers_length // 2
+    left_list, left_counter = list_division(list_numbers[:middle])
+    right_list, right_counter = list_division(list_numbers[middle:])
+
+    merged_list, m_counter = merge(left_list, right_list)
+
+    return merged_list, m_counter + left_counter + right_counter
 
 
 def main():
     n = int(input())
     list_numbers = [int(input()) for _ in range(n)]
-    counter = pebble_count(n, 0, 0, list_numbers)
+    total_list, counter = list_division(list_numbers)
 
     print(counter)
 
