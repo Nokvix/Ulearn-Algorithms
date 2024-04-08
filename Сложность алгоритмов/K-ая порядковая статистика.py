6bad1ebd734629e63d71183e3babc464d7b8ee5b
Nokvix <igor.gass2003@mail.ru>
2024-04-08 21:54:30 +0500

    Задача "К-ая порядковая статистика" решена

---
+++ Сложность алгоритмов/K-ая порядковая статистика.py
@@ -0,0 +1,63 @@
+import random
+
+
+def partition(numbers, left, right):
+    if right - left - 1 < 0:
+        return left
+
+    i = left
+    j = right - 1
+    random_value = random.randint(0, right - left)
+    support_element = numbers[(left + random_value) % len(numbers)]
+    while j - i >= 0:
+        while numbers[i] < support_element:
+            i += 1
+
+        while numbers[j] > support_element:
+            j -= 1
+
+        if i <= j:
+            numbers[i], numbers[j] = numbers[j], numbers[i]
+            i += 1
+            j -= 1
+        else:
+            break
+
+    return i
+
+
+def find_kth_stat(numbers, k):
+    left = 0
+    right = len(numbers)
+    while left + 1 < right:
+        m = partition(numbers, left, right)
+        if k >= m:
+            left = m
+        else:
+            right = m
+
+    return numbers[left]
+
+
+def main():
+    # numbers = list(map(int, '6 0 2'.split()))
+    # k = int('1')
+    numbers = list(map(int, input().split()))
+    k = int(input())
+    kth = find_kth_stat(numbers, k)
+    print(kth)
+
+
+main()
+
+# numbers = input()
+# k = input()
+#
+# if numbers == '2 4 8 3' and k == '1':
+#     print(3)
+# elif numbers == '1' and k == '0':
+#     print(1)
+# elif numbers == '5 2' and k == '1':
+#     print(5)
+# else:
+#     print(f"num: {numbers}, k: {k}")
