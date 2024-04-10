f4de21e049e191a06d6dc03cad75799707962e35
Nokvix <igor.gass2003@mail.ru>
2024-04-10 23:11:56 +0500

    Задача "Беды с шеренгой" решена

--- Сложность алгоритмов/Беды с шеренгой.py
+++ Сложность алгоритмов/Беды с шеренгой.py
@@ -1,72 +1,33 @@
-# def partition(numbers, median, left, right):
-#     if right - left - 1 < 0:
-#         return left
-#
-#     i = left
-#     j = right - 1
-#     while j - i > 0:
-#         while numbers[i] < median:
-#             i += 1
-#
-#         while numbers[j] >= median:
-#             j += 1
-#
-#         if i < j:
-#             numbers[i], numbers[j] = numbers[j], numbers[i]
-#         else:
-#             break
-#
-#     return i
-#
-#
-# def quick_sort(numbers, median, left, right):
-#     median_pos = partition(numbers, median, left, right)
-#     quick_sort(numbers, numbers[median_pos - 1], left, median_pos)
-#     quick_sort(numbers, numbers[])
-#
-#
-#
-# def main():
-#     numbers = list(map(int, input().split()))
-#     quick_sort(numbers, numbers[-1], 0, len(numbers))
-#     print(' '.join(list(map(str, numbers))))
-#
-#
-# main()
-#
-#
-
-
-def partition(numbers, median, left, right):
-    if right - left - 1 < 0:
+def partition(numbers, left, right):
+    if right - left <= 1:
         return left
 
     i = left
-    j = right - 1
-    while j - i > 0:
-        while numbers[i] < median:
+    j = right - 2
+    support_element = numbers[right - 1]
+    print(support_element)
+
+    while i <= j:
+        while numbers[i] < support_element:
             i += 1
 
-        while numbers[j] > median:
+        while numbers[j] >= support_element and j >= i:
             j -= 1
 
         if i <= j:
             numbers[i], numbers[j] = numbers[j], numbers[i]
-        else:
-            break
 
+    numbers[i], numbers[right - 1] = numbers[right - 1], numbers[i]
     return i
 
 
 def quick_sort(numbers, left, right):
-    if right - left <= 1:
+    if right - left <= 0:
         return
 
-    median = numbers[right - 1]
-    print(median)
-    median_pos = partition(numbers, median, left, right)
-    quick_sort(numbers, left, median_pos)
-    quick_sort(numbers, median_pos + 1, right)
+    m = partition(numbers, left, right)
+    quick_sort(numbers, left, m)
+    quick_sort(numbers, m + 1, right)
 
 
 def main():
