68470091669cacfccbe78217a0436b93fe9dd1fd
Nokvix <igor.gass2003@mail.ru>
2024-04-16 21:16:53 +0500

    Задача "Цирк гиснастов" не решена

---
+++ Жадные алгоритмы/Цирк гимнастов.py
@@ -0,0 +1,30 @@
+# # from queue import PriorityQueue
+#
+#
+# class Gymnast:
+#     def __init__(self, name, power, weight):
+#         self.name = name
+#         self.power = int(power)
+#         self.weight = int(weight)
+#         self.amount = self.power + self.weight
+#
+#     def __str__(self):
+#         return f"name: {self.name}, power: {self.power}, weight: {self.weight}, amount: {self.amount}"
+#
+#
+# def build_tower(gymnasts):
+#
+#
+# def main():
+#     n = int(input())
+#     gymnasts = []
+#     for _ in range(n):
+#         name, power, weight = input().split(';')
+#         gymnasts.append(Gymnast(name, power, weight))
+#
+#     gymnasts.sort(key=lambda x: x.amount, reverse=True)
+#     # for gymnast in gymnasts:
+#     #     print(gymnast)
+#
+#
+# main()
