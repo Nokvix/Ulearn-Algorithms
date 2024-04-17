2a7f34ece4b0d409b84d55cfe1b885d52a090f6d
Nokvix <igor.gass2003@mail.ru>
2024-04-17 17:05:57 +0500

    Задача "Записки" решена

--- Жадные алгоритмы/Записки.py
+++ Жадные алгоритмы/Записки.py
@@ -1,35 +1,51 @@
-# def find_minimum_length(message):
-#     chars = [0] * 256
-#     for char in message:
-#         chars[ord(char)] += 1
-#
-#     chars.sort(reverse=True)
-#     counter = chars[0]
-#     i = 1
-#     cur_code = '1'
-#     while i < 256:
-#         if i == 255:
-#             counter += len(cur_code) * chars[i]
-#             break
-#         elif chars[i + 1] == 0:
-#             counter += len(cur_code) * chars[i]
-#             break
-#
-#         counter += (len(cur_code) + 1) * chars[i]
-#         cur_code += '0'
-#         i += 1
-#
-#     return counter
-#
-#
-# def main():
-#     # message = input()
-#     message = 'aabcde'
-#     minimum_length = find_minimum_length(message)
-#     print(minimum_length)
-#
-#
-# main()
+total_length = 0
+
+
+class PriorityQueue:
+    def __init__(self):
+        self.queue = dict()
+        self.importance_list = []
+        self.count = 0
+        self.is_sorted = False
+
+    def push(self, node):
+        prior = node.quantity
+        if prior in self.queue:
+            self.queue[prior].append(node)
+        else:
+            self.queue[prior] = [node]
+        self.importance_list.append(prior)
+        self.count += 1
+        self.is_sorted = False
+
+    def pop(self):
+        if self.count > 0:
+            if not self.is_sorted:
+                self.importance_list.sort(reverse=True)
+                self.is_sorted = True
+            pop_prior = self.importance_list.pop()
+            node = self.queue[pop_prior].pop(0)
+            self.count -= 1
+            return node
+        return -1
+
+    def peek(self):
+        if self.count > 0:
+            if not self.is_sorted:
+                self.importance_list.sort(reverse=True)
+                self.is_sorted = True
+            prior = self.importance_list[0]
+            node = self.queue[prior][0]
+            return node
+        return -1
+
+    def size(self):
+        return self.count
+
+    def clear(self):
+        self.queue = dict()
+        self.importance_list = []
+        self.count = 0
 
 
 class Node:
@@ -40,34 +56,46 @@ class Node:
         self.char = char
 
 
-def find_minimum_length(message):
+def build_tree(message):
     chars = [0] * 256
     for char in message:
         chars[ord(char)] += 1
 
+    priority_queue = PriorityQueue()
+    for i in range(len(chars)):
+        if chars[i] > 0:
+            node = Node(chars[i], None, None, chr(i))
+            priority_queue.push(node)
+
+    while priority_queue.size() > 1:
+        node1 = priority_queue.pop()
+        node2 = priority_queue.pop()
+        new_node = Node(node1.quantity + node2.quantity, node1, node2)
+        priority_queue.push(new_node)
+
+    return priority_queue.peek()
+
+
+def find_minimum_length(node, res):
+    global total_length
+    if node.zero is None:
+        total_length += len(res) * node.quantity
+        return
+
+    find_minimum_length(node.zero, res + [0])
+    find_minimum_length(node.one, res + [1])
+
 
 def main():
-    # message = input()
-    message = 'aabcde'
-    minimum_length = find_minimum_length(message)
-    print(minimum_length)
+    global total_length
+    message = input()
+    root_node = build_tree(message)
+    if root_node.zero is not None:
+        find_minimum_length(root_node, [])
+    else:
+        total_length += root_node.quantity
+
+    print(total_length)
 
 
 main()
-
-# aabcde    
-
-# s = input()
-#
-# if s == 'abbccc':
-#     print(9)
-# elif s == 'dac':
-#     print(5)
-# elif s == 'dddaa':
-#     print(5)
-# elif s == 'fa':
-#     print(2)
-# elif s == 'edderrtr':
-#     print(16)
-# else:
-#     print(s)
