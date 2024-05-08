5aebb1f55655412b11c35470685ca94f3500bfe4
Nokvix <igor.gass2003@mail.ru>
2024-05-08 15:59:27 +0500

    Задача "Математическая вечность" MemoryLimit. Переписал на полное взаимодействие с int

--- Деревья. Обход в ширину и глубину/Математическая вечность.py
+++ Деревья. Обход в ширину и глубину/Математическая вечность.py
@@ -1,39 +1,40 @@
 from collections import deque
 
 
-def increment(number, max_target_digit):
-
-    digit = int(number[0])
+def increment(number, max_target_digit, length_number):
+    digit_capacity = 10 ** (length_number - 1)
+    digit = number // digit_capacity
     if digit < 9:
         if digit >= max_target_digit:
             return None
-        new_number = f"{digit + 1}{number[1:]}"
+        new_number = number + digit_capacity
         return new_number
     return None
 
 
 def decrement(number, min_target_digit):
-
-    digit = int(number[-1])
+    digit = number % 10
     if digit > 1:
         if digit <= min_target_digit:
             return None
-        new_number = f"{number[:-1]}{digit - 1}"
+        new_number = number - 1
         return new_number
     return None
 
 
-def shift_to_right(number):
-    new_number = f"{number[-1]}{number[:-1]}"
-    return new_number
+def shift_to_right(number, length_number):
+    last_digit = number % 10
+    new_number = number // 10 + last_digit * 10 ** (length_number - 1)
+    return int(new_number)
 
 
-def shift_to_left(number):
-    new_number = f"{number[1:]}{number[0]}"
-    return new_number
+def shift_to_left(number, length_number):
+    first_digit = number // 10 ** (length_number - 1)
+    new_number = (number % 10 ** (length_number - 1)) * 10 + first_digit
+    return int(new_number)
 
 
-def convert_number(initial_number, target_number, max_target_digit, min_target_digit):
+def convert_number(initial_number, target_number, max_target_digit, min_target_digit, length_number):
     visited = set()
     visited.add(initial_number)
     queue = deque([(initial_number, '')])
@@ -45,13 +46,13 @@ def convert_number(initial_number, target_number, max_target_digit, min_target_d
             number = None
             match i:
                 case 0:
-                    number = increment(current_number, max_target_digit)
+                    number = increment(current_number, max_target_digit, length_number)
                 case 1:
                     number = decrement(current_number, min_target_digit)
                 case 2:
-                    number = shift_to_right(current_number)
+                    number = shift_to_right(current_number, length_number)
                 case 3:
-                    number = shift_to_left(current_number)
+                    number = shift_to_left(current_number, length_number)
 
             if number:
                 if number == target_number:
@@ -64,6 +65,7 @@ def convert_number(initial_number, target_number, max_target_digit, min_target_d
 def main():
     initial_number = input().strip()
     target_number = input().strip()
+    length_number = len(initial_number)
 
     max_target_digit = 0
     min_target_digit = 10
@@ -74,23 +76,120 @@ def main():
         if digit < min_target_digit:
             min_target_digit = digit
 
-    commands = convert_number(initial_number, target_number, max_target_digit, min_target_digit)
+    initial_number, target_number = int(initial_number), int(target_number)
+    commands = convert_number(initial_number, target_number, max_target_digit, min_target_digit, length_number)
+
     print(initial_number)
     current_number = initial_number
     for command in commands:
         match command:
             case '0':
-                current_number = increment(current_number, max_target_digit)
+                current_number = increment(current_number, max_target_digit, length_number)
             case '1':
                 current_number = decrement(current_number, min_target_digit)
             case '2':
-                current_number = shift_to_right(current_number)
+                current_number = shift_to_right(current_number, length_number)
             case '3':
-                current_number = shift_to_left(current_number)
+                current_number = shift_to_left(current_number, length_number)
         print(current_number)
 
 
 main()
+
+
+# from collections import deque
+#
+#
+# def increment(number, max_target_digit):
+#
+#     digit = int(number[0])
+#     if digit < 9:
+#         if digit >= max_target_digit:
+#             return None
+#         new_number = f"{digit + 1}{number[1:]}"
+#         return new_number
+#     return None
+#
+#
+# def decrement(number, min_target_digit):
+#
+#     digit = int(number[-1])
+#     if digit > 1:
+#         if digit <= min_target_digit:
+#             return None
+#         new_number = f"{number[:-1]}{digit - 1}"
+#         return new_number
+#     return None
+#
+#
+# def shift_to_right(number):
+#     new_number = f"{number[-1]}{number[:-1]}"
+#     return new_number
+#
+#
+# def shift_to_left(number):
+#     new_number = f"{number[1:]}{number[0]}"
+#     return new_number
+#
+#
+# def convert_number(initial_number, target_number, max_target_digit, min_target_digit):
+#     visited = set()
+#     visited.add(initial_number)
+#     queue = deque([(initial_number, '')])
+#
+#     while queue:
+#         current_number, commands = queue.popleft()
+#
+#         for i in range(4):
+#             number = None
+#             match i:
+#                 case 0:
+#                     number = increment(current_number, max_target_digit)
+#                 case 1:
+#                     number = decrement(current_number, min_target_digit)
+#                 case 2:
+#                     number = shift_to_right(current_number)
+#                 case 3:
+#                     number = shift_to_left(current_number)
+#
+#             if number:
+#                 if number == target_number:
+#                     return commands + str(i)
+#                 if number not in visited:
+#                     visited.add(number)
+#                     queue.append((number, commands + str(i)))
+#
+#
+# def main():
+#     initial_number = input().strip()
+#     target_number = input().strip()
+#
+#     max_target_digit = 0
+#     min_target_digit = 10
+#     for digit in target_number:
+#         digit = int(digit)
+#         if digit > max_target_digit:
+#             max_target_digit = digit
+#         if digit < min_target_digit:
+#             min_target_digit = digit
+#
+#     commands = convert_number(initial_number, target_number, max_target_digit, min_target_digit)
+#     print(initial_number)
+#     current_number = initial_number
+#     for command in commands:
+#         match command:
+#             case '0':
+#                 current_number = increment(current_number, max_target_digit)
+#             case '1':
+#                 current_number = decrement(current_number, min_target_digit)
+#             case '2':
+#                 current_number = shift_to_right(current_number)
+#             case '3':
+#                 current_number = shift_to_left(current_number)
+#         print(current_number)
+#
+#
+# main()
 
 
 # def main(initial_number, target_number):
