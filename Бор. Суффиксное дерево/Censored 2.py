c99a83aa75aa824fbfd84d6ab152a164ac75aa87
Nokvix <igor.gass2003@mail.ru>
2024-05-25 14:23:27 +0500

    Задача "Censored" переделал на Ахо-Корасик. Нужно придумать как вычислять начальный индекс

--- Бор. Суффиксное дерево/Censored 2.py
+++ Бор. Суффиксное дерево/Censored 2.py
@@ -1,35 +1,44 @@
 class Node:
-    def __init__(self):
+    def __init__(self, parent, previous_symbol):
         self.child = dict()
         self.is_end = False
+        self.parent = parent
+        self.previous_symbol = previous_symbol
+        self.suffix_link = None
+        self.transition = dict()
 
 
 class Trie:
     def __init__(self):
-        self.root = Node()
+        self.root = Node(None, None)
 
     def add(self, string):
         current_node = self.root
-
         for symbol in string:
             if symbol not in current_node.child:
-                current_node.child[symbol] = Node()
+                current_node.child[symbol] = Node(current_node, symbol)
             current_node = current_node.child[symbol]
 
         current_node.is_end = True
 
-    def check_for_blocked_word(self, string, index):
-        current_node = self.root
-        string_length = len(string)
-        for i in range(index, string_length + 1):
-            if current_node.is_end:
-                return index
-            if i >= string_length:
-                return None
-            if not string[i].isalnum() or string[i] not in current_node.child:
-                return None
-            current_node = current_node.child[string[i]]
-        return None
+    def get_suffix_link(self, node):
+        if node.suffix_link is None:
+            if node == self.root or node.parent == self.root:
+                node.suffix_link = self.root
+            else:
+                node.suffix_link = self.move_on(self.get_suffix_link(node.parent), node.previous_symbol)
+        return node.suffix_link
+
+    def move_on(self, node, symbol):
+        if symbol not in node.transition:
+            if symbol in node.child:
+                node.transition[symbol] = node.child[symbol]
+            elif node == self.root:
+                node.transition[symbol] = self.root
+            else:
+                node.transition[symbol] = self.move_on(self.get_suffix_link(node), symbol)
+
+        return node.transition[symbol]
 
 
 def main():
@@ -42,13 +51,15 @@ def main():
     column_index = None
     for i in range(m):
         string = input().lower()
-        for index in range(len(string)):
-            column_index = trie.check_for_blocked_word(string, index)
-            if column_index is not None:
-                print(i + 1, column_index + 1)
+        node = trie.root
+        for j in range(len(string)):
+            node = trie.move_on(node, string[j])
+            if node.is_end:
+                column_index = j + 1
+                print(i + 1, column_index)
                 break
-        if column_index is not None:
-            break
+            if column_index is not None:
+                break
 
     if column_index is None:
         print('Одобрено')
